d704ae3a86a68621f97c49738d80b74dd2380fbc
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-03-09 11:31:33 +0800

    Update customize.sh

--- module/customize.sh
+++ module/customize.sh
@@ -1,18 +1,12 @@
 #!/system/bin/sh
 SKIPUNZIP=1
 
-CONFIG_DIR=/data/adb/boothashdisguiser
+CONFIG_DIR=/data/adb/vbmetadisguiser
+CONFIG_FILE="$CONFIG_DIR/vbmeta.conf"
 LOG_DIR="$CONFIG_DIR/logs"
-VERIFY_DIR="$TMPDIR/.aa_bhd_verify"
-
+VERIFY_DIR="$TMPDIR/.aa_vd_verify"
 MOD_NAME="$(grep_prop name "${TMPDIR}/module.prop")"
 MOD_VER="$(grep_prop version "${TMPDIR}/module.prop") ($(grep_prop versionCode "${TMPDIR}/module.prop"))"
-
-if [ ! -d "$LOG_DIR" ]; then
-  echo "- $LOG_DIR does not exist"
-  mkdir -p "$LOG_DIR" || abort "! Failed to create $LOG_DIR!"
-  echo "- Created $LOG_DIR"
-fi
 
 echo "- Extract aautilities.sh"
 unzip -o "$ZIPFILE" 'aautilities.sh' -d "$TMPDIR" >&2
@@ -20,41 +14,38 @@ if [ ! -f "$TMPDIR/aautilities.sh" ]; then
   echo "! Failed to extract aautilities.sh!"
   abort "! This zip may be corrupted!"
 fi
+
 . "$TMPDIR/aautilities.sh"
 
-module_install_proc(){
-  echo "- Setting up $MOD_NAME"
-  echo "- Version: $MOD_VER"
-
-  if [ "$API" -lt 27 ]; then
-    echo "! Android version is lower than 8(oreo)!"
-    about "$MOD_NAME does not support Android 8-!"
-  fi
-
-  echo "- Extract module file(s)"
-  extract "$ZIPFILE" 'aautilities.sh' "$VERIFY_DIR"
-  extract "$ZIPFILE" 'customize.sh' "$VERIFY_DIR"
-  extract "$ZIPFILE" 'module.prop' "$MODPATH"
-  extract "$ZIPFILE" 'service.sh' "$MODPATH"
-  extract "$ZIPFILE" 'uninstall.sh' "$MODPATH"
-  if [ ! -d "$CONFIG_DIR" ]; then
-    mkdir -p "$CONFIG_DIR" || abort "! Failed to create directory: $CONFIG_DIR"
-  fi
-  if [ ! -f "$CONFIG_DIR/hash.info" ]; then
-    echo "- hash.info does not exist"
-    extract "$ZIPFILE" 'hash.info' "$TMPDIR"
-    mv "$TMPDIR/hash.info" "$CONFIG_DIR/hash.info" || abort "! Failed to create hash.info!"
-    echo "- Created hash.info"
-    echo "- PLEASE COPY BOOT HASH INTO THE FILE: $CONFIG_DIR/hash.info !"
-    echo "- AND THEN REBOOT TO TAKE EFFECT!"
-  else
-    echo "- Detect hash.info already existed"
-    echo "- Skip overwriting hash.info"
-  fi
-}
-
+logowl "Setting up $MOD_NAME"
+logowl "Version: $MOD_VER"
+init_logowl "$LOG_DIR"
 show_system_info
-install_env_check
-module_install_proc
+install_env_check 
+clean_old_logs "$LOG_DIR" 20
+
+if [ "$API" -lt 27 ]; then
+    logowl "Detect Android version is lower than 8 (oreo)!" "ERROR"
+    logowl "$MOD_NAME does not support Android 8 and lower"
+    about "since VBMeta does NOT exist in these old Android version!"
+fi
+
+logowl "Extract module files"
+extract "$ZIPFILE" 'aautilities.sh' "$VERIFY_DIR"
+extract "$ZIPFILE" 'customize.sh' "$VERIFY_DIR"
+extract "$ZIPFILE" 'aautilities.sh' "$MODPATH"
+extract "$ZIPFILE" 'module.prop' "$MODPATH"
+extract "$ZIPFILE" 'service.sh' "$MODPATH"
+extract "$ZIPFILE" 'uninstall.sh' "$MODPATH"
+
+if [ ! -f "$CONFIG_DIR/vbmeta.conf" ]; then
+    logowl "vbmeta.conf does not exist" "TIPS"
+    extract "$ZIPFILE" 'vbmeta.conf' "$TMPDIR"
+    mv "$TMPDIR/vbmeta.conf" "$CONFIG_DIR/vbmeta.conf" || abort "! Failed to create vbmeta.conf!"
+else
+    logowl "Detect vbmeta.conf already existed"
+    logowl "Skip overwriting vbmeta.conf"
+fi
+
 set_module_files_perm
-echo "- Welcome to use ${MOD_NAME}!"
+logowl "Welcome to use ${MOD_NAME}!"
