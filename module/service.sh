a7a722e0ab3f93cedf1f74d9c0ff268f21876463
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-03-12 10:14:31 +0800

    Update service.sh

--- module/service.sh
+++ module/service.sh
@@ -9,34 +9,39 @@ MOD_NAME="$(sed -n 's/^name=\(.*\)/\1/p' "${MODDIR}/module.prop")"
 MOD_AUTHOR="$(sed -n 's/^author=\(.*\)/\1/p' "${MODDIR}/module.prop")"
 MOD_VER="$(sed -n 's/^version=\(.*\)/\1/p' "${MODDIR}/module.prop") ($(sed -n 's/^versionCode=\(.*\)/\1/p' "${MODDIR}/module.prop"))"
 
+AVB_VERSION="2.0"
+VBMETA_SIZE="4096"
+BOOT_HASH="00000000000000000000000000000000"
+
 config_loader() {
 
     logowl "Start loading configuration"
-
-    boot_hash=$(init_variables "boot_hash" "$CONFIG_FILE")
+    
+    avb_version=$(init_variables "avb_version" "$CONFIG_FILE")
     vbmeta_size=$(init_variables "vbmeta_size" "$CONFIG_FILE")
+    boot_hash=$(init_variables "boot_hash" "$CONFIG_FILE")
 
-    verify_variables "boot_hash" "$boot_hash" "^[0-9a-fA-F]{64}$"
+    verify_variables "avb_version" "$avb_version" "^[1-9][0-9]*\.[0-9]*$|^[1-9][0-9]*$"
     verify_variables "vbmeta_size" "$vbmeta_size" "^[1-9][0-9]*$"
+    verify_variables "boot_hash" "$boot_hash" "^[0-9a-fA-F]{64}$"
 
 }
 
 . "$MODDIR/aautilities.sh"
 
+module_intro >> "$LOG_FILE"
 init_logowl "$LOG_DIR"
-install_env_check
-module_intro >> "$LOG_FILE" 
 logowl "Starting service.sh"
 config_loader >> "$LOG_FILE"
 print_line >> "$LOG_FILE"
 
 resetprop "ro.boot.vbmeta.device_state" "locked"
-resetprop "ro.boot.vbmeta.avb_version" "2.0"
 resetprop "ro.boot.vbmeta.hash_alg" "sha256"
 
 if [ -s "$CONFIG_FILE" ]; then
     resetprop "ro.boot.vbmeta.digest" "$BOOT_HASH"
     resetprop "ro.boot.vbmeta.size" "$VBMETA_SIZE"
+    resetprop "ro.boot.vbmeta.avb_version" "$AVB_VERSION"
 fi
 
 print_line >> "$LOG_FILE"
@@ -49,7 +54,5 @@ logowl "ro.boot.vbmeta.digest=$(ro.boot.vbmeta.digest)"
 print_line >> "$LOG_FILE"
 
 logowl "Variables before case closed"
-print_line >> "$LOG_FILE"
 debug_print_values >> "$LOG_FILE"
-print_line >> "$LOG_FILE"
 logowl "service.sh case closed!"
