ecd8603a2e13c69fb16c439560b812008761d8c4
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-03-18 12:37:32 +0800

    Update service.sh

--- module/service.sh
+++ module/service.sh
@@ -1,19 +1,24 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
 
-CONFIG_DIR=/data/adb/vbmetadisguiser
+CONFIG_DIR="/data/adb/vbmetadisguiser"
+
 CONFIG_FILE="$CONFIG_DIR/vbmeta.conf"
 LOG_DIR="$CONFIG_DIR/logs"
 LOG_FILE="$LOG_DIR/vd_log_core_$(date +"%Y-%m-%d_%H-%M-%S").log"
-MOD_NAME="$(sed -n 's/^name=\(.*\)/\1/p' "${MODDIR}/module.prop")"
-MOD_AUTHOR="$(sed -n 's/^author=\(.*\)/\1/p' "${MODDIR}/module.prop")"
-MOD_VER="$(sed -n 's/^version=\(.*\)/\1/p' "${MODDIR}/module.prop") ($(sed -n 's/^versionCode=\(.*\)/\1/p' "${MODDIR}/module.prop"))"
+
+MODULE_PROP="${MODDIR}/module.prop"
+MOD_NAME="$(sed -n 's/^name=\(.*\)/\1/p' "$MODULE_PROP")"
+MOD_AUTHOR="$(sed -n 's/^author=\(.*\)/\1/p' "$MODULE_PROP")"
+MOD_VER="$(sed -n 's/^version=\(.*\)/\1/p' "$MODULE_PROP") ($(sed -n 's/^versionCode=\(.*\)/\1/p' "$MODULE_PROP"))"
 
 AVB_VERSION="2.0"
 VBMETA_SIZE="4096"
 BOOT_HASH="00000000000000000000000000000000"
 
 config_loader() {
+    # config_loader: a function to load the config file saved in $CONFIG_FILE
+    # the format of $CONFIG_FILE: value=key, one key-value pair per line
 
     logowl "Start loading configuration"
     
@@ -29,26 +34,39 @@ config_loader() {
 
 }
 
+vbmeta_disguiser() {
+
+    resetprop -n "ro.boot.vbmeta.device_state" "locked"
+    resetprop -n "ro.boot.vbmeta.hash_alg" "sha256"
+
+    if [ -s "$CONFIG_FILE" ]; then
+        resetprop -n "ro.boot.vbmeta.digest" "$BOOT_HASH"
+        resetprop -n "ro.boot.vbmeta.size" "$VBMETA_SIZE"
+        resetprop -n "ro.boot.vbmeta.avb_version" "$AVB_VERSION"
+    fi
+
+}
+
+encryption_disguiser(){
+
+    if [ -n "$CRYPTO_STATE" ]; then
+        resetprop -n "ro.crypto.state" "$CRYPTO_STATE"
+    fi
+
+}
+
 . "$MODDIR/aautilities.sh"
 
 init_logowl "$LOG_DIR"
 module_intro >> "$LOG_FILE"
-logowl "Starting service.sh"
-config_loader >> "$LOG_FILE"
+show_system_info
 print_line >> "$LOG_FILE"
-
-resetprop -n "ro.boot.vbmeta.device_state" "locked"
-resetprop -n "ro.boot.vbmeta.hash_alg" "sha256"
-
-if [ -s "$CONFIG_FILE" ]; then
-    resetprop -n "ro.boot.vbmeta.digest" "$BOOT_HASH"
-    resetprop -n "ro.boot.vbmeta.size" "$VBMETA_SIZE"
-    resetprop -n "ro.boot.vbmeta.avb_version" "$AVB_VERSION"
-    if [ -n "$CRYPTO_STATE" ]; then
-        resetprop -n "ro.crypto.state" "$CRYPTO_STATE"
-    fi
-fi
-
+logowl "Starting service.sh"
+print_line >> "$LOG_FILE"
+config_loader
+vbmeta_disguiser
+encryption_disguiser
+print_line >> "$LOG_FILE"
 logowl "Specific variables"
 print_line >> "$LOG_FILE"
 logowl "ro.boot.vbmeta.device_state=$(getprop ro.boot.vbmeta.device_state)"
@@ -58,7 +76,6 @@ logowl "ro.boot.vbmeta.size=$(getprop ro.boot.vbmeta.size)"
 logowl "ro.boot.vbmeta.digest=$(getprop ro.boot.vbmeta.digest)"
 logowl "ro.crypto.state=$(getprop ro.crypto.state)"
 print_line >> "$LOG_FILE"
-
 logowl "Variables before case closed"
 debug_print_values >> "$LOG_FILE"
 logowl "service.sh case closed!"
