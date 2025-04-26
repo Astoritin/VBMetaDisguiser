0339ecebc9823070874b37a981a39e40dff40a80
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-04-26 11:58:14 +0800

    Update uninstall.sh

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -1,8 +1,1 @@
-#!/system/bin/sh
-MODDIR=${0%/*}
-
-CONFIG_DIR="/data/adb/vbmetadisguiser"
-
-if [ -n "$CONFIG_DIR" ] && [ -d "$CONFIG_DIR" ] && [ "$CONFIG_DIR" != "/" ]; then
-    rm -rf "$CONFIG_DIR"
-fi
+rm -rf /data/adb/vbmetadisguiser
