380fb25d98b2cb15853bb14c600f2574e48d14a9
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-05-17 23:26:11 +0800

    Update post-fs-data.sh

--- module/post-fs-data.sh
+++ module/post-fs-data.sh
@@ -5,45 +5,15 @@ CONFIG_DIR="/data/adb/vbmetadisguiser"
 
 CONFIG_FILE="$CONFIG_DIR/vbmeta.conf"
 LOG_DIR="$CONFIG_DIR/logs"
-LOG_FILE="$LOG_DIR/vd_spcfg_$(date +"%Y-%m-%d_%H-%M-%S").log"
+LOG_FILE="$LOG_DIR/vd_sp_$(date +"%Y-%m-%d_%H-%M-%S").log"
 
 MODULE_PROP="${MODDIR}/module.prop"
 MOD_NAME="$(sed -n 's/^name=\(.*\)/\1/p' "$MODULE_PROP")"
 MOD_AUTHOR="$(sed -n 's/^author=\(.*\)/\1/p' "$MODULE_PROP")"
 MOD_VER="$(sed -n 's/^version=\(.*\)/\1/p' "$MODULE_PROP") ($(sed -n 's/^versionCode=\(.*\)/\1/p' "$MODULE_PROP"))"
 
-MN_SUPPORT=false
-MR_SUPPORT=false
-
 TRICKY_STORE_CONFIG_FILE="/data/adb/tricky_store/security_patch.txt"
 SECURITY_PATCH_DATE=""
-
-config_loader() {
-
-    logowl "Load config"
-
-    if [ "$DETECT_KSU" = true ] || [ "$DETECT_APATCH" = true ]; then
-        logowl "$MOD_NAME is running on KernelSU / APatch"
-        logowl "Make Node mode support is present"
-        MN_SUPPORT=true
-        MR_SUPPORT=false
-
-    elif [ "$DETECT_MAGISK" = true ]; then
-        logowl "$MOD_NAME is running on Magisk"
-        logowl "Magisk Replace mode support is present"
-        MR_SUPPORT=true
-        if [ $MAGISK_V_VER_CODE -ge 28102 ]; then
-            logowl "$MOD_NAME is running on Magisk 28102+"
-            logowl "Make Node mode support is present"
-            MN_SUPPORT=true
-        else
-            logowl "Make Node mode requires Magisk version 28102+!" "WARN"
-            logowl "$MOD_NAME will revert to Magisk Replace mode"
-            MN_SUPPORT=false
-        fi
-    fi
-
-}
 
 debug_props_info() {
 
@@ -60,10 +30,7 @@ date_format_86() {
     key_name=$1
     key_date_value=$2
 
-    if [ -z "$key_name" ] || [ -z "$key_date_value" ]; then
-        logowl "Key name or date is NULL!" "ERROR"
-        return 1
-    fi
+    [ -z "$key_name" ] || [ -z "$key_date_value" ] && return 1
 
     if echo "$key_date_value" | grep -qE '^[0-9]{8}$'; then
         formatted_date=$(echo "$key_date_value" | sed 's/\([0-9]\{4\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)/\1-\2-\3/')
@@ -84,49 +51,54 @@ date_format_86() {
     fi
 }
 
-mirror_make_node() {
+date_format_86() {
+    key_name="$1"
+    key_date_value="$2"
 
-    node_path=$1
+    [ -z "$key_name" ] || [ -z "$key_date_value" ] && return 1
 
-    if [ -z "$node_path" ]; then
-        logowl "node_path is NOT ordered!" "ERROR"
-        return 5
-    elif [ ! -e "$node_path" ]; then
-        logowl "$node_path does NOT exist!" "ERROR"
-        return 6
-    fi
+    len=$(echo "$key_date_value" | awk '{print length}')
+    format_success=false
 
-    node_path_parent_dir=$(dirname "$node_path")
-    mirror_parent_dir="$MODDIR$node_path_parent_dir"
-    mirror_node_path="$MODDIR$node_path"
+    case "$len" in
+        8)
+            if echo "$key_date_value" | grep -qE '^[0-9]{8}$'; then
+                formatted_date=$(echo "$key_date_value" | sed 's/\([0-9]\{4\}\)\([0-9]\{2\}\)\([0-9]\{2\}\)/\1-\2-\3/')
+                format_success=true
+            fi
+            ;;
+        6)
+            if echo "$key_date_value" | grep -qE '^[0-9]{6}$'; then
+                formatted_date=$(echo "$key_date_value" | sed 's/\([0-9]\{4\}\)\([0-9]\{2\}\)/\1-\2/')
+                format_success=true
+            fi
+            ;;
+        10)
+            if echo "$key_date_value" | grep -qE '^[0-9]{4}-[0-9]{2}-[0-9]{2}$'; then
+                logowl "Current key $key_name=$key_date_value has been formatted already" "WARN"
+                return 0
+            fi
+            ;;
+        7)
+            if echo "$key_date_value" | grep -qE '^[0-9]{4}-[0-9]{2}$'; then
+                logowl "Current key $key_name=$key_date_value has been formatted already" "WARN"
+                return 0
+            fi
+            ;;
+        *)
+            logowl "Invalid date format: $key_date_value" "ERROR"
+            return 2
+            ;;
+    esac
 
-    if [ ! -d "$mirror_parent_dir" ]; then
-        logowl "Parent dir $mirror_parent_dir does NOT exist"
-        mkdir -p "$mirror_parent_dir"
-        logowl "Create parent dir: $mirror_parent_dir"
-    fi
+    [ "$format_success" = false ] && { logowl "Invalid date format: $key_date_value" "ERROR"; return 2; }
 
-    if [ ! -e "$mirror_node_path" ]; then
-        logowl "Node $mirror_node_path does NOT exist"
-        mknod "$mirror_node_path" c 0 0
-        result_make_node="$?"
-        logowl "Execute: mknod $mirror_node_path c 0 0"
-        if [ $result_make_node -eq 0 ]; then
-            return 0
-        else
-            return $result_make_node
-        fi
-    else
-        logowl "Node $mirror_node_path exists already"
-        return 0
-    fi
-
+    eval "$key_name=\"$formatted_date\""
+    logowl "Set $key_name=$formatted_date" "TIPS"
 }
 
 ts_sp_config_simple() {
-
     logowl "Load security patch config (simple mode)"
-
     patch_level=$(grep -v '^#' "$TRICKY_STORE_CONFIG_FILE" | grep -Eo '[0-9]+' | head -n 1)
 
     if [ -z "$patch_level" ]; then
@@ -224,184 +196,12 @@ security_patch_info_disguiser() {
     fi
 }
 
-custom_install_recovery_script_remove() {
-
-    logowl "Start custom install-recovery.sh script removal process"
-
-    block_install_recovery_script=$(init_variables "block_install_recovery_script" "$CONFIG_FILE")
-    block_install_recovery_script_mode=$(init_variables "block_install_recovery_script_mode" "$CONFIG_FILE")
-    custom_install_recovery_script_path=$(init_variables "custom_install_recovery_script_path" "$CONFIG_FILE")
-
-    if [ -z "$custom_install_recovery_script_path" ]; then
-        logowl "Custom install-recovery.sh path is NOT set" "ERROR"
-        return 2
-    fi
-
-    WORK_MODE=""
-    if [ "$block_install_recovery_script" = "false" ] || [ -z "$block_install_recovery_script" ]; then
-        logowl "Custom install-recovery.sh removal feature is disabled"
-        return 1
-    elif [ "$block_install_recovery_script" = "true" ]; then
-
-        if [ -z "$block_install_recovery_script_mode" ]; then
-            logowl "Block install-recovery.sh mode is NOT set!" "ERROR"
-            return 1
-        fi
-
-        if [ "$block_install_recovery_script_mode" = "MN" ]; then
-            if [ "$MN_SUPPORT" = false ]; then
-                logowl "Make Node mode needs Magisk version 28102 and higher, KernelSU or APatch!" "ERROR"
-                logowl "However, either ED (Erase/Delete) or RN (Rename) mode is NOT systemless behavior"
-                logowl "$MOD_NAME will skip these step directly and will NOT switch into ED or RN mode"
-                logowl "Please adjust this option manually if need"
-                return 2
-            fi
-        elif [ "$block_install_recovery_script_mode" = "RN" ] || [ "$block_install_recovery_script_mode" = "ED" ]; then
-            if [ "$block_install_recovery_script_mode" = "RN" ]; then
-                WORK_MODE="RN (Rename)"
-            elif [ "$block_install_recovery_script_mode" = "ED" ]; then
-                WORK_MODE="DE (Delete/Erase)"
-            fi
-            logowl "$WORK_MODE mode is NOT systemless behavior" "WARN"
-            logowl "If your file system is read-only, it will NOT work at all...well it is okay"
-            logowl "But if NOT, please make sure you know how to rescue from your device being brick!"
-            logowl "Or make sure you have the method to modify the files located in /system partition by OrangeFox/TWRP"
-            logowl "This option/method must be switched by yourself ONLY"
-            logowl "You have been warned before you change the mode!" "WARN"
-        else
-            logowl "Abnormal block install-recovery.sh mode status!" "ERROR"
-            return 1
-        fi
-    else
-        logowl "Abnormal block install-recovery.sh switch status!" "ERROR"
-        return 1
-    fi
-
-    IFS=' '
-    SWITCH_TO_MN=false
-    for ins_rec_sh in $custom_install_recovery_script_path; do
-        if [ -f "$ins_rec_sh" ]; then
-            if [ "$block_install_recovery_script_mode" = "RN" ]; then
-                logowl "Rename $ins_rec_sh → ${ins_rec_sh}.old"
-                if ! mv -n "$ins_rec_sh" "${ins_rec_sh}.old"; then
-                    logowl "Rename failed (code: $?)" "ERROR";
-                    [ "$MN_SUPPORT" = true ] && SWITCH_TO_MN=true
-                else
-                    continue
-                fi
-            elif [ "$block_install_recovery_script_mode" = "ED" ]; then
-                logowl "Delete file: $ins_rec_sh"
-                if ! rm -f "$ins_rec_sh"; then
-                    logowl "Deletion failed (code: $?)" "ERROR";
-                    [ "$MN_SUPPORT" = true ] && SWITCH_TO_MN=true
-                else
-                    continue
-                fi
-            fi
-
-            if [ "$block_install_recovery_script_mode" = "MN" ] || [ "$SWITCH_TO_MN" = true ] ; then
-                mirror_make_node "$ins_rec_sh" || { logowl "Failed to make node (code: $?)" "ERROR"; }
-            fi
-
-        else
-            logowl "File not found: $ins_rec_sh" "WARN"
-        fi
-    done
-    unset IFS
-}
-
-custom_addon_d_remove() {
-
-    logowl "Start custom addon.d removal process"
-
-    block_addon_d_dir=$(init_variables "block_addon_d_dir" "$CONFIG_FILE")
-    block_addon_d_dir_mode=$(init_variables "block_addon_d_dir_mode" "$CONFIG_FILE")
-    custom_addon_d_path=$(init_variables "custom_addon_d_path" "$CONFIG_FILE")
-
-    if [ -z "$custom_addon_d_path" ]; then
-        logowl "Custom addon.d path is NOT set" "ERROR"
-        return 2
-    fi
-
-    WORK_MODE=""
-    if [ "$block_addon_d_dir" = "false" ] || [ -z "$block_addon_d_dir" ]; then
-        logowl "Custom addon.d removal feature is disabled"
-        return 1
-    elif [ "$block_addon_d_dir" = "true" ]; then
-        
-        if [ -z "$block_addon_d_dir_mode" ]; then
-            logowl "Block addon.d mode is NOT set!" "ERROR"
-            return 1
-        fi
-
-        if [ "$block_addon_d_dir_mode" = "MN" ]; then
-            if [ "$MN_SUPPORT" = false ]; then
-                logowl "Make Node mode needs Magisk version 28102 and higher, KernelSU or APatch!" "ERROR"
-                logowl "However, either ED (Erase/Delete) or RN (Rename) mode is NOT systemless behavior"
-                logowl "$MOD_NAME will skip these step directly and will NOT switch into ED or RN mode"
-                logowl "Please adjust this option manually if need"
-                return 2
-            fi
-        elif [ "$block_addon_d_dir_mode" = "RN" ] || [ "$block_addon_d_dir_mode" = "ED" ]; then
-            if [ "$block_addon_d_dir_mode" = "RN" ]; then
-                WORK_MODE="RN (Rename)"
-            elif [ "$block_addon_d_dir_mode" = "ED" ]; then
-                WORK_MODE="DE (Delete/Erase)"
-            fi
-            logowl "$WORK_MODE mode is NOT systemless behavior" "WARN"
-            logowl "If your file system is read-only, it will NOT work at all...well it is okay"
-            logowl "But if NOT, please make sure you know how to rescue from your device being brick!"
-            logowl "Or make sure you have the method to modify the files located in /system partition by OrangeFox/TWRP"
-            logowl "This option/method must be switched by yourself ONLY"
-            logowl "You have been warned before you change the mode!" "WARN"
-        else
-            logowl "Abnormal block addon.d mode status!" "ERROR"
-            return 1
-        fi
-    else
-        logowl "Abnormal block addon.d switch status!" "ERROR"
-        return 1
-    fi
-
-    IFS=' '
-    SWITCH_TO_MN=false
-    for addon_d in $custom_addon_d_path; do
-        if [ -f "$addon_d" ]; then
-            if [ "$block_addon_d_dir_mode" = "RN" ]; then
-                logowl "Rename $addon_d → ${addon_d}.old"
-                if ! mv -n "$addon_d" "${addon_d}.old"; then
-                    logowl "Rename failed (code: $?)" "ERROR";
-                    [ "$MN_SUPPORT" = true ] && SWITCH_TO_MN=true
-                else
-                    continue
-                fi
-            elif [ "$block_addon_d_dir_mode" = "ED" ]; then
-                logowl "Delete file: $addon_d"
-                if ! rm -f "$addon_d"; then
-                    logowl "Deletion failed (code: $?)" "ERROR";
-                    [ "$MN_SUPPORT" = true ] && SWITCH_TO_MN=true
-                else
-                    continue
-                fi
-            fi
-            
-            if [ "$block_addon_d_dir_mode" = "MN" ] || [ "$SWITCH_TO_MN" = true ]; then
-                mirror_make_node "$addon_d" || { logowl "Failed to make node (code: $?)" "ERROR"; }
-            fi
-
-        else
-            logowl "Dir not found: $addon_d" "WARN"
-        fi
-    done
-
-}
 
 . "$MODDIR/aautilities.sh"
 
 init_logowl "$LOG_DIR"
 module_intro >> "$LOG_FILE"
 show_system_info
-config_loader
 print_line
 logowl "Start post-fs-data.sh"
 print_line
@@ -411,7 +211,5 @@ security_patch_info_disguiser
 print_line
 logowl "After:"
 debug_props_info
-custom_install_recovery_script_remove
-custom_addon_d_remove
 print_line
 logowl "post-fs-data.sh case closed!"
