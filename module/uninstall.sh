78b86acc6f44c47d10c5e273b97b5d6524f31897
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-06-16 11:20:30 +0800

    Update uninstall.sh

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -1,6 +1,5 @@
 CONFIG_DIR="/data/adb/vbmetadisguiser"
-LOG_DIR="$CONFIG_DIR/logs"
-SLAIN_PROPS="$LOG_DIR/slain_props.prop"
+SLAIN_PROPS="$CONFIG_DIR/slain_props.prop"
 
 [ -f "$SLAIN_PROPS" ] && resetprop -p -f "$SLAIN_PROPS"
 
